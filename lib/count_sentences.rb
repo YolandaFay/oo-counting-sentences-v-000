87499b421140d0d333d974976d625e108b21b026
Yolanda Fay <yolandafay@gmail.com>
2018-10-13 23:50:33 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -19,7 +19,7 @@ class String
     sentence_array = self.split(/\s/)
     sentence_array.each do |word|
       if word.question? || word.sentence? || word.exclamation?
-        count=+1 
+        count+=1 
         binding.pry
       end
     end
